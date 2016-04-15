103ebd44c348dfe89616724acf2c4caf2592e4ac
Dylan K Tran <dtran2386@gmail.com>
2016-04-14 22:17:29 -0400

    grid created. pages route automatically upon game start and game end.

--- js/views/play.js
+++ js/views/play.js
@@ -1,33 +1,28 @@
 // AS = array specific syntax
 module.exports = Backbone.View.extend({
     initialize: function() {
-//        this.model.randomPosition(); // why won't this work while randomEnergy() is also invoked at the same time?
         this.model.on('change', this.render, this);
-//        console.log('listening for change event');
     },
     events: {
-        'click #click-up': 'clickUp',
-        'click #click-down': 'clickDown',
         'click #click-right': 'clickRight',
         'click #click-left': 'clickLeft',
+        'click #click-up': 'clickUp',
+        'click #click-down': 'clickDown',
     },
     template: _.template(document.getElementById('play-game-template').textContent),
     render: function() {
-//        console.log('info changed');
-//        this.el.innerHTML = this.model.get('currentPositions'); //AS
-//        var content = []; //AS
-        
+
+        if (this.model.get('energy') < this.model.get('minEnergy')) {
+            alert('Game Over!');
+            window.location.href = 'file:///Users/dtranrules/Desktop/dailyhw/day32/public/index.html?car=on#/end';
+        }
+        if (this.model.get('x') === this.model.get('xEnergy') && this.model.get('y') === this.model.get('yEnergy'))         {
+            this.model.randomEnergy();
+            alert('You picked up a random stranger. Congratulations.');
+            this.model.set('podsCollected', this.model.get('podsCollected') + 1);
+            this.model.set('totalScore', this.model.get('totalScore') + 29);
+        }
         var playaz = this.model.get('name');
-//        for (var i = 0; i < playaz.length; i++) { //AS
-//            var html = this.template({
-//                name: playaz[i].name,
-//                position: playaz[i].position,
-//            });
-////            console.log(html);
-//            content.push(html); //AS
-//        }
-        
-//        console.log(content.join(''));
         var x = this.model.get('x');
         var y = this.model.get('y');
         var car = this.model.get('vehicle');
@@ -35,6 +30,7 @@ module.exports = Backbone.View.extend({
         var xEnergy = this.model.get('xEnergy');
         var yEnergy = this.model.get('yEnergy');
         var podsCollected = this.model.get('podsCollected');
+        var totalScore = this.model.get('totalScore');
         this.el.innerHTML = this.template({
             name: playaz,
             x: x,
@@ -44,26 +40,21 @@ module.exports = Backbone.View.extend({
             xEnergy: xEnergy,
             yEnergy: yEnergy,
             podsCollected: podsCollected,
+            totalScore: totalScore,
         });
-        if (this.model.get('energy') < 1) {
-            prompt('Game Over! Would you play this game again?');
-            alert('Aiite Aiite I c u');
-        }
-        if (this.model.get('x') === this.model.get('xEnergy') && this.model.get('y') === this.model.get('yEnergy')) {
-            this.model.randomEnergy();
-            alert('got em!');
-            this.model.set('podsCollected', this.model.get('podsCollected') + 1);
-        }
-    },
-    clickUp: function () {
-        console.log('clicked up');
-        this.model.up();
-        this.model.energyLoss();
-    },
-    clickDown: function () {
-        console.log('clicked down');
-        this.model.down();
-        this.model.energyLoss();
+        var grid = document.getElementById('cell-grid');
+        for (var x = 0; x < 10; x++) {
+          for (var y = 0; y < 10; y++) {
+              var div = document.createElement('div');
+              div.classList.add('cells');
+              
+              // is the current cell an energy pod?
+              if (x === this.model.get('xEnergy') && y === this.model.get('yEnergy')) {
+                div.classList.add('energy-pod');
+              }
+              grid.appendChild(div);
+          }; // end inner for loop
+        }; // end outer for loop
     },
     clickRight: function () {
         console.log('clicked right');
@@ -73,6 +64,16 @@ module.exports = Backbone.View.extend({
     clickLeft: function () {
         console.log('clicked left');
         this.model.left();
+        this.model.energyLoss();
+    },
+    clickUp: function () {
+        console.log('clicked up');
+        this.model.up();
+        this.model.energyLoss();
+    },
+    clickDown: function () {
+        console.log('clicked down');
+        this.model.down();
         this.model.energyLoss();
     },
 });
